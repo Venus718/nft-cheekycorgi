8cda5835ea57c15f894a268585831e2e0255245a
venus718 <venusdragon718@gmail.com>
2021-11-24 23:50:29 +0900

    fix: claim status update

--- pages/claim.js
+++ pages/claim.js
@@ -25,6 +25,7 @@ export default function Claim() {
   const [pendingClaim, setPendingClaim] = useState(false)
   const [claimableNftContracts, setClaimableNftContracts] = useState([])
   const [totalClaimableNftCounts, setTotalClaimableNftCounts] = useState(0)
+  const [claimableNftCounts, setClaimableNftCounts] = useState([])
 
   const onClickClaim = async () => {
     console.log('total claimed: ', totalClaimed)
@@ -33,15 +34,15 @@ export default function Claim() {
     if (!totalClaimableNftCounts || totalClaimed >= maxClaimable) {
       return NotificationManager.error('All claimed already!')
     }
-    console.log('claiming cheekycorgi ...')
+    console.log('Claiming cheekycorgi ...')
 
     setPendingClaim(true)
     try {
       for(let _collectionId = 0; _collectionId < claimableNftContracts.length; _collectionId ++) {
         let _collection = claimableNftContracts[_collectionId]
 
-        if (_collection.balanceOf > 0) {
-          for (let _index = 0; _index < _collection.balanceOf; _index ++) {
+        if (claimableNftCounts[_collectionId] > 0) {
+          for (let _index = 0; _index < claimableNftCounts[_collectionId]; _index ++) {
             let _tokenId = await _collection.contract.methods.tokenOfOwnerByIndex(wallet.address, _index).call()
             let _isClaimedAlready = await nftContract.methods.isClaimedAlready(_collectionId, _tokenId).call()
             if (!_isClaimedAlready) {
@@ -77,7 +78,7 @@ export default function Claim() {
       console.log('total claimed: ', _totalClaimed)
       console.log('max claimable: ', _maxClaimable)
 
-      let _claimableNftContracts = [], _totalBalance = 0
+      let _claimableNftContracts = [], _totalBalance = 0, _balances = []
       for (let _contractAddress of FRIENDSHIP_NFT_ADDRESSES) {
         let _contract = new web3.eth.Contract(erc721EnumerableABI, _contractAddress)
         let _balanceOf = await _contract.methods.balanceOf(wallet.address).call()
@@ -86,12 +87,13 @@ export default function Claim() {
         _claimableNftContracts.push({
           address: _contractAddress,
           contract: _contract,
-          balanceOf: Number(_balanceOf)
         })
+        _balances.push(Number(_balanceOf))
       }
 
       setClaimableNftContracts(_claimableNftContracts)
       setTotalClaimableNftCounts(_totalBalance)
+      setClaimableNftCounts(_balances)
 
       console.log('created claimable contracts: ', _claimableNftContracts.length)
       console.log('claimable nft balance: ', _totalBalance)
@@ -113,14 +115,16 @@ export default function Claim() {
       setTotalClaimed(Number(_totalClaimed))
       console.log('total claimed: ', _totalClaimed)
   
-      let _totalBalance = 0
+      let _totalBalance = 0, _balances = []
       for (let _collection of claimableNftContracts) {
         let _balanceOf = await _collection.contract.methods.balanceOf(wallet.address).call()
         console.log('fetching balanceOf: ', _collection.address, _balanceOf)
         _totalBalance += Number(_balanceOf)
+        _balances.push(Number(_balanceOf))
       }
       console.log('claimable nft balance: ', _totalBalance)
       setTotalClaimableNftCounts(_totalBalance)
+      setClaimableNftCounts(_balances)
     }
 
     if (nftContract && claimableNftContracts.length > 0) {

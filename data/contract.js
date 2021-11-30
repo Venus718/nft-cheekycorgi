1f14209e8fc6bf7791b6ed28c67405894165e0f0
venus718 <venusdragon718@gmail.com>
2021-12-01 01:13:44 +0900

    update address

--- data/contract.js
+++ data/contract.js
@@ -6,8 +6,8 @@ export const ACTIVE_NETWORK_ID = 4
 // ==========================================================================================
 
 // below is contract address in rinkeby
-export const NFT_CONTRACT_ADDRESS = '0xeaeC34E0A8aF481C8309E17C13A4Cf55b645171d'
-export const YIELD_TOKEN_CONTRACT_ADDRESS = '0xaEb63cF65342144EA11e2B81A43f91f11c5B6b03'
+export const NFT_CONTRACT_ADDRESS = '0x5ff84F491Ac6AA7b089ba69eDC6e3b98230671b0'
+export const YIELD_TOKEN_CONTRACT_ADDRESS = '0xDeb95Ce9d8e8bc7d432be0f20080BCa67D30bEa8'
 
 // don't change the order, keep it same with Smart Contract
 export const FRIENDSHIP_NFT_ADDRESSES = ['0xC675Fb458a155C1af6F373057Dde1DD00cdd3494',
